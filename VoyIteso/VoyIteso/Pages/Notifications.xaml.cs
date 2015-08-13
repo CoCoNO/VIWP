74b4abcd05bc86087c6c9814d02e59edd681355b
Sergio Pimienta <pimienta.sergio@gmail.com>
2015-08-13 15:56:51 -0500

    mas movimientos en la gui
    se agregan cajas en notificaciones con el boton del appbar, para
    pruebas.

--- VoyIteso/VoyIteso/Pages/Notifications.xaml.cs
+++ VoyIteso/VoyIteso/Pages/Notifications.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
@@ -22,17 +23,39 @@ namespace VoyIteso.Pages
 
         private void CrearNuevaCajitaFeliz_click(object sender, EventArgs e)
         {
+            //pinche trampa sucia! pero funciona y se ve muy bien. 
             var grid = new Grid();
-            grid.Width = 400;
-            grid.Height = 150;
-            grid.Background = new SolidColorBrush(Color.FromArgb(255, 133, 187, 220));
+            grid.Width = 440;
+            grid.Height = 20;
+            lista.Items.Add(grid);
+
+            //Este es el grid importante. 
+
+
+
+            grid = new Grid();
+            grid.Width = 440;
+            grid.Height = 112;//150
+            grid.Background = new SolidColorBrush(Color.FromArgb(255, 133, 187, 220));//este es el color de prueba. 
+
+            var sgrid = new Grid();
+            sgrid.Margin = new Thickness(10,0,366,0);//10,0,366,0
+            var img = new Image();
+            var bmp = new BitmapImage();
+            var uir = new Uri("Images/you.jpg", UriKind.Relative);//("ms-appx:///Images/you.png");
+            bmp.UriSource = uir;
+            img.Source = bmp;
+            sgrid.Children.Add(img);
 
             var sp = new StackPanel();
+            sp.Margin = new Thickness(91,0,0,0);
             var tb = new TextBlock();
+            tb.FontSize = 13;
             tb.Text = "header";
             var tb2 = new TextBlock();
             tb2.Text = "content";
 
+            sp.Children.Add(sgrid);
             sp.Children.Add(tb);
             sp.Children.Add(tb2);
 
