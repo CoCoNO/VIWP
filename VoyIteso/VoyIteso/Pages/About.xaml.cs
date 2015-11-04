e45d52bd9c392bf3d5039dcaa2123d0d550edb79
Sergio Pimienta <pimienta.sergio@gmail.com>
2015-11-04 09:06:08 -0600

    about page

---
+++ VoyIteso/VoyIteso/Pages/About.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace VoyIteso.Pages
+{
+    public partial class About : PhoneApplicationPage
+    {
+        public About()
+        {
+            InitializeComponent();
+
+            this.versionnumber.Text += Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        }
+    }
+}
